69272deee9f423a17e45f6757f9ec0bd3f92dc97
hseonkang-MN2 <hseonkang-mn2@hseonkang-MN2ui-MacBookPro.local>
2022-08-12 09:35:45 +0900

    modify webpack config

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,7 @@ import './main-style.css';
 import { DocumentSelectionExample } from './component/document-selection-example';
 
 const excute = () => {
-    const macroCallback = () => console.log('macro task: called macro task timeout callback.');
-
-    const microCallback = () => {
-        console.log('micro task: called micro task queueMicrotask callback. first');
-        console.log('micro task: called micro task queueMicrotask callback. second');
-    };
-
-    const microTimeoutCallback = () => setTimeout(() => console.log('micro settimeout'), 0);
-
-    const promiseCallback = () => console.log('called Promise callback');
-    
-    queueMicrotask(microTimeoutCallback);
-    setTimeout(macroCallback, 0);
-    queueMicrotask(microCallback);
-    Promise.resolve().then(() => promiseCallback());
+    const documentSelectionExample = new DocumentSelectionExample();
 };
 
 excute();
